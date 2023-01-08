5efb8c237aac75f675d5b72a87b69fce8a5fa7ca
Leonardo-Sanchez-Salomon <121777250+Leonardo-Sanchez-Salomon@users.noreply.github.com>
2023-01-08 13:17:18 -0600

    Add files via upload
    Snake Game started. Current files create body of snake, puts snake in a constant state of movement, and allows user to change the snakes direction using arrow keys.

---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.turties = []
+        self.create_snake()
+        self.head = self.turties[0]
+
+    def create_snake(self):
+        for turty_pos in STARTING_POS:
+            turt = Turtle("square")
+            turt.penup()
+            turt.goto(turty_pos)
+            turt.color("white")
+            self.turties.append(turt)
+
+    def move(self):
+
+        for num in range(len(self.turties) - 1, 0, -1):
+            new_x = self.turties[num - 1].xcor()
+            new_y = self.turties[num - 1].ycor()
+            self.turties[num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
